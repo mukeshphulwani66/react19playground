642dac6d2b555b9781263013172020cf19519dab
Mukesh <mukeshphulwani66@gmail.com>
2024-07-28 15:12:20 +0530

    added loading

--- src/components/UseHookExample2.js
+++ src/components/UseHookExample2.js
@@ -7,6 +7,7 @@ function UseHookExample2() {
     const [loading, setLoading] = useState(true);
 
     async function fetchData() {
+        setLoading(true)
         const response = await fetch('https://jsonplaceholder.typicode.com/users');
         const result = await response.json();
         setUserData(result[0]);
